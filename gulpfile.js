284f50dbd44418ee71cbe1f44c2b54e9c369c526
fisker Cheung <lionkay@gmail.com>
2021-06-29 18:08:27 +0800

    Meta tweak

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require('gulp-autoprefixer')
 const cleanCSS = require('gulp-clean-css')
 const sourcemaps = require('gulp-sourcemaps')
